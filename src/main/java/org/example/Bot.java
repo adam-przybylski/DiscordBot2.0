e0c63f531e6e9f5a79e1ca31469d14a4b29bde2e
adam-przybylski <adamprzybylski789@gmail.com>
2023-08-07 18:47:01 +0200

    Added .env file

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+
 public class Bot {
+
+    private final Dotenv config;
+    private final ShardManager shardManager;
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+
+    public Bot() throws LoginException {
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.listening("Żaluzja - Spiwór"));
+        shardManager = builder.build();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            Bot bot = new Bot();
+        } catch (LoginException e) {
+            System.err.println("ERROR: Provided bot token is invalid");
+        }
     }
 }
